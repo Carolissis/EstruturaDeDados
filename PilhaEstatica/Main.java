117fcffc995fb206ddf72224360d316100ada497
Caroline Assis <caroolassissss@gmail.com>
2024-08-09 20:39:35 -0300

    atividade static stack (pilha estatica)

---
+++ PilhaEstatica/Main.java
@@ -0,0 +1,34 @@
+
+public class Main{
+    public static void main(String []args){
+
+        StaticStack<Integer> pilha = new StaticStack<>(5);
+
+        pilha.clear();
+        pilha.push(1);
+        pilha.push(2);
+        System.out.println(pilha.toString());
+        pilha.pop();
+        System.out.println(pilha.toString());
+        pilha.push(4);
+        pilha.push(5);
+        System.out.println(pilha.toString());
+        pilha.pop();
+        pilha.pop();
+        System.out.println(pilha.toString());
+        pilha.push(6);
+        pilha.push(7);
+        pilha.push(8);
+        System.out.println(pilha.toString());
+        pilha.clear();
+        System.out.println(pilha.toString());
+        pilha.push(9);
+        pilha.push(9);
+        pilha.push(9);
+        pilha.push(9);
+        pilha.push(9);
+        pilha.push(9);
+        pilha.push(9);
+        System.out.println(pilha.toString());
+    }
+}
